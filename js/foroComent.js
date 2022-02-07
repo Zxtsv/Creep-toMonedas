6ff187880120eae04fc754a69ec13e28fdb0375a
Joceline Marquez <andrea.marquezvivaldo@gmail.com>
2022-02-06 20:02:31 -0600

    Cambio por bootstrap

--- js/foroComent.js
+++ js/foroComent.js
@@ -1,11 +1,9 @@
-const open = document.getElementById('open');
-const modal_container = document.getElementById('modal_container');
-const close = document.getElementById('close');
-
-open.addEventListener('click', () => {
-  modal_container.classList.add('show');  
-});
-
-close.addEventListener('click', () => {
-  modal_container.classList.remove('show');
-});
+$('#exampleModal').on('show.bs.modal', function (event) {
+    var button = $(event.relatedTarget) // Button that triggered the modal
+    var recipient = button.data('whatever') // Extract info from data-* attributes
+    // If necessary, you could initiate an AJAX request here (and then do the updating in a callback).
+    // Update the modal's content. We'll use jQuery here, but you could use a data binding library or other methods instead.
+    var modal = $(this)
+    modal.find('.modal-title').text('New message to ' + recipient)
+    modal.find('.modal-body input').val(recipient)
+})
