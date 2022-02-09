686123a22c2be3bb847af571db9950df08c12ba6
Zxtsv <bernardo_pprz@comunidad.unam.mx>
2022-02-09 11:48:23 -0600

    Añadi otro HTML y modifique el main.js

--- js/main.js
+++ js/main.js
@@ -1,11 +1,2 @@
-$(window).on("scroll", function() {
-    if($(window).scrollTop()) {
-          $('nav').addClass('navbar-scrolled');
-    }
-
-    else {
-          $('nav').removeClass('navbar-scrolled');
-    }
-});
 
 ScrollReveal().reveal('.dashboard-foro', { delay: 20 });
