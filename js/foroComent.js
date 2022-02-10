1ef156a2753c1121883760bc606dead93c306fa7
Zxtsv <bernardo_pprz@comunidad.unam.mx>
2022-02-09 22:19:46 -0600

    Añadi columnas al foro

--- js/foroComent.js
+++ js/foroComent.js
@@ -17,7 +17,6 @@ var postsWallets = [];
 
 function enviars() {
     // usaremos join para unir los arrays y unshif para agregar el primer elemento arriba
-
     let title = "<h3>";
     let category = selectCategory();
     title += document.getElementById("comment-title").value + category + "</h3>"; // Extrae el valor del titulo
@@ -25,13 +24,16 @@ function enviars() {
     doubt += document.getElementById("message-text").value + "</p>"; // Extrae el valor de la duda
 
     let post =
-        "<div class='col-md-9'><div class='right-description893'><div id='que-hedder2983'>" +
+        "<div class='col-md-9 m-0 p-0'><div class='right-description893'><div id='que-hedder2983'>" +
         title +
         "</div><div class='ques-details10018'>" +
         doubt +
         "</div><hr></div></div>";
 
     posts.unshift(post);
+
+    unshifPosts(post);
+
     let result = posts.join("");
     document.getElementById("section-publication").innerHTML = result;
     alerta();
@@ -41,18 +43,57 @@ function alerta() {
     Swal.fire('Tu publicación se ha publicado correctamente');
 }
 
-function selectCategory(){
+// Filtro Function
+function unshifPosts(post) {
+    let selection = document.getElementById("exampleFormControlSelect1");
+    switch (selection.selectedIndex) {
+        case 0:
+            postsBlockchain.unshift(post);
+            break;
+        case 1:
+            postsCripto.unshift(post);
+            break;
+        case 2:
+            postsWallets.unshift(post);
+            break;
+        default:
+            break;
+    }
+}
+
+function innerCategory(index) {
+    let resultado="";
+    switch (index) { 
+        case 0:
+            resultado = postsBlockchain.join("");
+            break;
+        case 1:
+            resultado = postsCripto.join("");
+            break;
+        case 2:
+            resultado = postsWallets.join("");
+            break;
+        case 3:
+            resultado = posts.join("");
+            break;
+        default:
+            break;
+    }
+    document.getElementById("section-publication").innerHTML = resultado;
+}
+
+function selectCategory() {
     let selection = document.getElementById("exampleFormControlSelect1");
     let boton = "";
     switch (selection.selectedIndex) {
         case 0:
-            boton = "<button type='button' class='btn btn-success ms-4 btn-redondeado' id='bttn-categoria'>"+ selection.options[selection.selectedIndex].value +"</button>";
+            boton = "<button type='button' class='btn btn-success ms-4 btn-redondeado' id='bttn-categoria'>" + selection.options[selection.selectedIndex].value + "</button>";
             break;
         case 1:
-            boton = "<button type='button' class='btn btn-info ms-4 btn-redondeado' id='bttn-categoria'>"+ selection.options[selection.selectedIndex].value +"</button>";
+            boton = "<button type='button' class='btn btn-info ms-4 btn-redondeado' id='bttn-categoria'>" + selection.options[selection.selectedIndex].value + "</button>";
             break;
         case 2:
-            boton = "<button type='button' class='btn btn-dark ms-4 btn-redondeado' id='bttn-categoria'>"+ selection.options[selection.selectedIndex].value +"</button>";
+            boton = "<button type='button' class='btn btn-dark ms-4 btn-redondeado' id='bttn-categoria'>" + selection.options[selection.selectedIndex].value + "</button>";
             break;
         default:
             boton = "";
@@ -61,3 +102,4 @@ function selectCategory(){
 
     return boton;
 }
+
